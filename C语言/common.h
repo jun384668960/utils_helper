3a0fe2009b84248a91000fe34fa2a3381edb41b2
Donyj <Donyj_ljj@163.com>
2018-02-08 15:00:10 +0800

    日志 信号量，互斥锁 cmap cqueue

---
+++ C语言/common.h
@@ -0,0 +1,33 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+#include <pthread.h> 
+#include <semaphore.h>
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+///////////////////////////////////////////////////////////////////
+//–≈∫≈¡ø
+typedef void* CSem;
+CSem  csem_create(int initial_count, int maxcount);
+int   csem_delete(CSem handle);
+int   csem_wait(CSem handle, unsigned int timeout/*∫¡√Î*/);
+int   csem_post(CSem handle);
+int   csem_getcount(CSem handle, int *count);
+
+
+///////////////////////////////////////////////////////////////////
+//ª•≥‚À¯
+typedef void* CMtx;
+CMtx cmtx_create();
+void cmtx_delete(CMtx handle);
+void cmtx_enter(CMtx handle);
+void cmtx_leave(CMtx handle);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
